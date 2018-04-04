f2a8ff9fed8f83ad5afdda3eacdb10391325782c
fdrury <fdrury@ualberta.ca>
2018-04-04 16:25:28 -0600

    Updated packet object

--- code/BT_Packet.cpp
+++ code/BT_Packet.cpp
@@ -1,1 +1,23 @@
-//object used to manage packet to be sent
+#include "BT_Packet.h"
+
+BT_Packet::BT_Packet(){
+    updatePending = false;
+}
+
+void BT_Packet::setGPS(uint8_t lat, uint8_t lng){
+    this.lat = lat;
+    this.lng = lng;
+    updatePending = true;
+}
+
+bool updatesPending(){
+    return updatePending;
+}
+
+uint32_t getPacket(){
+    uint32_t packet;
+    packet = UUID << (4 * 8);
+    packet &= lat << (2 * 8);
+    packet &= lng;
+    return packet;
+}
---
+++ code/BT_Packet.h
@@ -0,0 +1,18 @@
+#ifndef BT_PACKET_H
+#define BT_PACKET_H
+
+
+class Dash {
+    public:
+        void setGPS(uint8_t lat, uint8_t lng);
+        bool updatesPending();
+        uint32_t getPacket();
+    protected:
+        uint16_t UUID;
+        uint8_t lat;
+        uint8_t lon;
+        bool updatePending;
+};
+
+
+#endif
