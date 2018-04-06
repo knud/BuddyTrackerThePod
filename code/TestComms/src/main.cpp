2e9aa53a8822af7a41576c63b4cc794724048f21
fdrury <fdrury@ualberta.ca>
2018-04-06 13:19:11 -0600

    added PlatformIO project

---
+++ code/TestComms/src/main.cpp
@@ -0,0 +1,11 @@
+#include <Arduino.h>
+#include "BT_Packet.h"
+#include "Buddy.h"
+
+void setup() {
+    // put your setup code here, to run once:
+}
+
+void loop() {
+    // put your main code here, to run repeatedly:
+}
