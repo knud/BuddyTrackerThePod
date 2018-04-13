f8f63e34fc4c03b10979772420a4b156a715ede2
fdrury <fdrury@ualberta.ca>
2018-04-13 11:07:00 -0600

    Tested orintation stuff unsuccessfully
    Also started working on LoRa reading

--- code/BuddyTrackerBasic/src/main.cpp
+++ code/BuddyTrackerBasic/src/main.cpp
@@ -5,7 +5,9 @@
 #include "BT_Packet.h"
 #include "Buddy.h"
 
-LinkedList<Buddy> buddies = LinkedList<Buddy>();
+
+LinkedList<Buddy> buddies;
+
 
 void setup() {
     Serial.begin(9600);
@@ -20,6 +22,50 @@ void setup() {
     }
 }
 
-void loop() {
 
+void loop() {
+    // TODO: collision avoidance
+    
+    // parse for a packet, and call onReceive with the result:
+    onReceive(LoRa.parsePacket());
+}
+
+
+void onReceive(int packetSize) {
+  if (packetSize == 0) return;          // if there's no packet, return
+
+  uint16_t UUID = LoRa.read();
+  
+  // read packet header bytes:
+  int recipient = LoRa.read();          // recipient address
+  byte sender = LoRa.read();            // sender address
+  byte incomingMsgId = LoRa.read();     // incoming msg ID
+  byte incomingLength = LoRa.read();    // incoming msg length
+
+  String incoming = "";
+
+  while (LoRa.available()) {
+    incoming += (char)LoRa.read();
+  }
+
+  if (incomingLength != incoming.length()) {   // check length for error
+    Serial.println("error: message length does not match length");
+    return;                             // skip rest of function
+  }
+
+  // if the recipient isn't this device or broadcast,
+  if (recipient != localAddress && recipient != 0xFF) {
+    Serial.println("This message is not for me.");
+    return;                             // skip rest of function
+  }
+
+  // if message is for this device, or broadcast, print details:
+  Serial.println("Received from: 0x" + String(sender, HEX));
+  Serial.println("Sent to: 0x" + String(recipient, HEX));
+  Serial.println("Message ID: " + String(incomingMsgId));
+  Serial.println("Message length: " + String(incomingLength));
+  Serial.println("Message: " + incoming);
+  Serial.println("RSSI: " + String(LoRa.packetRssi()));
+  Serial.println("Snr: " + String(LoRa.packetSnr()));
+  Serial.println();
 }
