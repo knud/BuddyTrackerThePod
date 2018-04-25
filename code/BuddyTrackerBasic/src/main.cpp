38a34d3fa5ad9849f0518d512b7c5f934b480bc1
fdrury <fdrury@ualberta.ca>
2018-04-24 21:45:20 -0600

    refined GPS function

--- code/BuddyTrackerBasic/src/main.cpp
+++ code/BuddyTrackerBasic/src/main.cpp
@@ -150,14 +150,9 @@ void startGPS(){
 }
 
 
-// TODO: this is the default Adafruit code, modify to actually update values
+// TODO: this is largely derived from example code, ensure it works
+// TODO: maybe handle with interupt instead?
 void handleGPS(){
-    // read data from the GPS in the 'main loop'
-    char c = GPS.read();
-    // if you want to debug, this is a good time to do it!
-    if (GPSECHO)
-        if (c) Serial.print(c);
-    // if a sentence is received, we can check the checksum, parse it...
     if (GPS.newNMEAreceived()) {
         // a tricky thing here is if we print the NMEA sentence, or data
         // we end up not listening and catching other sentences!
@@ -169,29 +164,16 @@ void handleGPS(){
     // if millis() or timer wraps around, we'll just reset it
     if (timer > millis()) timer = millis();
         
-    // approximately every 2 seconds or so, print out the current stats
+    // approximately every 2 seconds or so, update lat and lng values
     if (millis() - timer > 2000) {
-        timer = millis(); // reset the timer
-        Serial.print("\nTime: ");
-        Serial.print(GPS.hour, DEC); Serial.print(':');
-        Serial.print(GPS.minute, DEC); Serial.print(':');
-        Serial.print(GPS.seconds, DEC); Serial.print('.');
-        Serial.println(GPS.milliseconds);
-        Serial.print("Date: ");
-        Serial.print(GPS.day, DEC); Serial.print('/');
-        Serial.print(GPS.month, DEC); Serial.print("/20");
-        Serial.println(GPS.year, DEC);
-        Serial.print("Fix: "); Serial.print((int)GPS.fix);
-        Serial.print(" quality: "); Serial.println((int)GPS.fixquality);
+        Serial.print("Satellites: "); Serial.println((int)GPS.satellites);
         if (GPS.fix) {
+            myLat = GPS.latitude;
+            myLng = GPS.longitude;
             Serial.print("Location: ");
             Serial.print(GPS.latitude, 4); Serial.print(GPS.lat);
             Serial.print(", ");
             Serial.print(GPS.longitude, 4); Serial.println(GPS.lon);
-            Serial.print("Speed (knots): "); Serial.println(GPS.speed);
-            Serial.print("Angle: "); Serial.println(GPS.angle);
-            Serial.print("Altitude: "); Serial.println(GPS.altitude);
-            Serial.print("Satellites: "); Serial.println((int)GPS.satellites);
         }
     }
 }
